229dfd0095a642eca564be3988497d655cc33d31
Nishchay Deep <nishchaydeep2@nutanix.com>
2025-08-11 20:26:41 +0530

    Day 5 commit

--- handler/taskhandler.go
+++ handler/taskhandler.go
@@ -4,12 +4,16 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
+	"sync"
 
 	"github.com/nishchaydeep15/go-task-api/model"
 	"github.com/nishchaydeep15/go-task-api/storage"
 )
 
-var tasks []model.Task
+var (
+	tasks []model.Task
+	mutex sync.Mutex
+)
 
 func init() {
 	loadedTasks, err := storage.LoadTasks()
@@ -32,20 +36,41 @@ func AddTask(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Task name cannot be empty", http.StatusBadRequest)
 		return
 	}
+	mutex.Lock()
 	tasks = append(tasks, task)
 	storage.SaveTasks(tasks)
+	mutex.Unlock()
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(map[string]string{"Message": "Task added"})
 }
 
 func ListTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content Type", "Application/json")
-	json.NewEncoder(w).Encode(tasks)
+
+	completed := r.URL.Query().Get("completed")
+	search := strings.TrimSpace(r.URL.Query().Get("search"))
+	// fmt.Println("Search query:", search)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	var filteredTasks []model.Task
+	for _, task := range tasks {
+		// fmt.Println("Checking task:", task.Name)
+		if (completed == "" || (completed == "true" && task.Completed) || (completed == "false" && !task.Completed)) &&
+			(search == "" || strings.Contains(strings.ToLower(task.Search), strings.ToLower(search))) {
+			filteredTasks = append(filteredTasks, task)
+		}
+	}
+	json.NewEncoder(w).Encode(filteredTasks)
 }
 
 func GetTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content Type", "Application/json")
 	name := r.URL.Query().Get("name")
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	for _, task := range tasks {
 		if strings.EqualFold(task.Name, name) {
 			json.NewEncoder(w).Encode(task)
@@ -59,6 +84,9 @@ func DeleteTask(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Task name is required", http.StatusBadRequest)
 		return
 	}
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	for index, task := range tasks {
 		if strings.EqualFold(strings.TrimSpace(task.Name), strings.TrimSpace(name)) {
 			tasks = append(tasks[:index], tasks[index+1:]...)
--- model/task.go
+++ model/task.go
@@ -1,5 +1,7 @@
 package model
 
 type Task struct {
-	Name string `json:"name"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed"`
+	Search    string `json:"search"`
 }
